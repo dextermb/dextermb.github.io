3b9d367d75a9afc162fefa7d182c92e8c7773d0b
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2018-12-11 20:37:24 +0000

    Add page title

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Page from '../components/page';
+import { Helmet } from 'react-helmet';
 
+import Page from '../components/page';
 import '../styles/app.scss';
 
 export default ({ children }) => (
     <Page>
+        <Helmet>
+            <title>Dexter Marks-Barber</title>
+        </Helmet>
         {children}
     </Page>
 )
